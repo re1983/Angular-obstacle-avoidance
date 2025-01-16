a28610e724324645483c77dfd20463904fdbadbf
re1983 <re1983@msn.com>
2025-01-15 18:33:39 -0700

    Lab PC

--- Bearing Rate Graph.py
+++ Bearing Rate Graph.py
@@ -49,13 +49,13 @@ def get_Angular_diameter(ship1, ship2):
     return np.degrees(angular_diameter)
 
 
-def adj_ownship_heading_velocity(bearings, angular_sizes, ship):
+# def adj_ownship_heading_velocity(bearings, angular_sizes, ship):
 
-    heading = (bearings[-1] - 90) % 360
-    # heading = ship.heading
-    velocity = ship.speed
+#     heading = (bearings[-1] - 90) % 360
+#     # heading = ship.heading
+#     velocity = ship.speed
 
-    return heading, velocity
+#     return heading, velocity
 
 def angle_difference(angle1, angle2):
     """
@@ -82,13 +82,41 @@ def angle_difference_in_deg(angle1, angle2):
 
     return angle_diff
 
+
+def adj_ownship_rate_of_turn(bearings_difference, angular_size, ship, goal, delta_time=0.01):
+
+    if len(bearings_difference) > 1 and angular_size > 7.5:
+        rate_of_turn = -(1/bearings_difference[-1]) / delta_time
+        # if abs(bearings_difference[-1]) > 0:
+        #     rate_of_turn = (1/bearings_difference[-1]) / delta_time
+        # else:
+        #     rate_of_turn = ship.rate_of_turn
+
+    #     print(bearings_difference[-1])
+    #     if abs(bearings_difference[-1]) > 0:
+    #         rate_of_turn = (1/bearings_difference[-1]) / delta_time
+    #     else:
+    #         rate_of_turn = 10 * bearings_difference[-1]
+        # if abs(bearings_difference[-1]) > abs(bearings_difference[-2]):
+        #     rate_of_turn = 0
+        # else:
+        #     rate_of_turn = -(1/bearings_difference[-1]) * 10
+
+    else:
+        sita_goal = get_bearing(ship, goal)
+        print('sita_goal:', sita_goal)
+        rate_of_turn = angle_difference_in_deg(ship.heading, sita_goal) * 0.3
+        print('rate_of_turn:', rate_of_turn)
+    return rate_of_turn
+
 # Example usage
 #name, speed, acceleration, heading, Rate_of_Turn, position
 # ownship = ShipStatus("Ownship", speed=5.0, acceleration=0, heading=0.0, Rate_of_Turn=-0.0, position=[-50, 0, 0])
 # ship = ShipStatus("Ship A", speed=7.07, acceleration=0, heading=135.0, Rate_of_Turn=-0.0, position=[50, -50, 0])
 
-ownship = ShipStatus("Ownship", speed=0.5, acceleration=0, heading=0.0, rate_of_turn=0.0, position=[-10, 0, 0])
-ship = ShipStatus("Ship A", speed=1, acceleration=0, heading=-90.0, rate_of_turn=0, position=[5, -5, 0])
+ownship = ShipStatus("Ownship", speed=0.99, acceleration=0, heading=0.0, rate_of_turn=-0.0, position=[-0, 0, 0])
+ship = ShipStatus("Ship A", speed=1.0, acceleration=0, heading=-90.0, rate_of_turn=0, position=[5, 5, 0])
+goal = ShipStatus("Goal", speed=0.0, acceleration=0, heading=0.0, rate_of_turn=0.0, position=[10, 0, 0])
 
 # Simulation parameters
 time_steps = 1000
@@ -100,6 +128,7 @@ ship_positions = []
 bearings = []
 angular_sizes = []
 bearings_difference = []
+distances = []
 # goal = ShipStatus("Ship A", speed=0.0, acceleration=0, heading=0.0, Rate_of_Turn=0.0, position=[0, 0, 0])
 # bearings_to_goal = []
 
@@ -113,9 +142,11 @@ for _ in range(time_steps):
     bearings.append(bearing)
     angular_size = get_Angular_diameter(ownship, ship)
     angular_sizes.append(angular_size)
+    distances.append(get_distance_3d(ownship.position, ship.position))
     # bearing_to_goal = get_bearing(ownship, goal)
     # bearings_to_goal.append(bearing_to_goal)
     # print("Bearing to Ship A:", bearing)
+    ownship.rate_of_turn = adj_ownship_rate_of_turn(bearings_difference, angular_size, ownship, goal, delta_time)
     ownship_positions.append(ownship.position.copy())
     ship_positions.append(ship.position.copy())
     # adj_ownship_heading_velocity(bearings, angular_sizes, ship)
@@ -134,8 +165,36 @@ for _ in range(time_steps):
 ownship_positions = np.array(ownship_positions)
 ship_positions = np.array(ship_positions)
 
-plt.figure(figsize=(10, 12))
-plt.subplot(5, 1, 1)
+plt.figure(figsize=(20, 24))
+
+plt.subplot(6, 1, 1)
+
+ownship_line, = plt.plot(ownship_positions[:, 1], ownship_positions[:, 0], label='Ownship')
+ship_line,   = plt.plot(ship_positions[:, 1],  ship_positions[:, 0],  label='Ship A')
+
+# Add arrow for Ownship
+plt.annotate(
+    '',
+    xy=(ownship_positions[-1, 1], ownship_positions[-1, 0]),
+    xytext=(ownship_positions[-2, 1], ownship_positions[-2, 0]),
+    arrowprops=dict(arrowstyle='->', color=ownship_line.get_color())
+)
+
+# Add arrow for Ship A
+plt.annotate(
+    '',
+    xy=(ship_positions[-1, 1], ship_positions[-1, 0]),
+    xytext=(ship_positions[-2, 1], ship_positions[-2, 0]),
+    arrowprops=dict(arrowstyle='->', color=ship_line.get_color())
+)
+
+plt.xlabel('East (m)')
+plt.ylabel('North (m)')
+plt.title('Ship Positions Over Time in NED Coordinates (Rotated 90 Degrees)')
+plt.legend()
+plt.grid(True)
+plt.axis('equal')  # Ensure the aspect ratio is equal
+
 
 # Plotting
 # plt.figure(figsize=(10, 6))
@@ -150,14 +209,14 @@ plt.subplot(5, 1, 1)
 
 # # Rotate the plot by 90 degrees
 # plt.figure(figsize=(10, 6))
-plt.plot(ownship_positions[:, 1], ownship_positions[:, 0], label='Ownship')
-plt.plot(ship_positions[:, 1], ship_positions[:, 0], label='Ship A')
-plt.xlabel('East (m)')
-plt.ylabel('North (m)')
-plt.title('Ship Positions Over Time in NED Coordinates (Rotated 90 Degrees)')
-plt.legend()
-plt.grid(True)
-plt.axis('equal')  # Ensure the aspect ratio is equal
+# plt.plot(ownship_positions[:, 1], ownship_positions[:, 0], label='Ownship')
+# plt.plot(ship_positions[:, 1], ship_positions[:, 0], label='Ship A')
+# plt.xlabel('East (m)')
+# plt.ylabel('North (m)')
+# plt.title('Ship Positions Over Time in NED Coordinates (Rotated 90 Degrees)')
+# plt.legend()
+# plt.grid(True)
+# plt.axis('equal')  # Ensure the aspect ratio is equal
 
 
 # Convert bearings to numpy array for easier plotting
@@ -169,7 +228,7 @@ bearing_difference = np.array(bearings_difference)
 # print(angular_sizes)
 # bearings = np.unwrap(bearings, period=360)  # Unwrap the bearings to remove jumps
 # Plotting the bearing rate
-plt.subplot(5, 1, 2)
+plt.subplot(6, 1, 2)
 plt.plot(bearings, np.arange(time_steps) * delta_time, label='Bearing to Ship A')
 half_angular_sizes = angular_sizes / 2
 plt.plot(bearings - half_angular_sizes, np.arange(time_steps) * delta_time)
@@ -192,7 +251,7 @@ plt.grid(True)
 # # plt.gca().invert_yaxis()  # Invert the Y-axis
 
 #ploting angular_sizes
-plt.subplot(5, 1, 3)
+plt.subplot(6, 1, 3)
 plt.plot(np.arange(time_steps) * delta_time, angular_sizes, label='Angular Size of Ship A')
 plt.xlabel('Time (s)')
 plt.ylabel('Angular Size (degrees)')
@@ -200,34 +259,45 @@ plt.title('Angular Size of Ship A Over Time')
 plt.legend()
 plt.grid(True)
 
-bearing_rate = np.gradient(bearings, delta_time)
-# Plotting the bearing rate
-plt.subplot(5, 1, 4)
-plt.plot(np.arange(time_steps) * delta_time, bearing_rate, label='Bearing Rate to Ship A')
+#position distance
+plt.subplot(6, 1, 4)
+plt.plot(np.arange(time_steps) * delta_time, distances, label='Distance to Ship A')
 plt.xlabel('Time (s)')
-plt.ylabel('Bearing Rate (degrees/s)')
-plt.title('Bearing Rate to Ship A Over Time')
+plt.ylabel('Distance (m)')
+plt.title('Distance to Ship A Over Time')
 plt.legend()
 plt.grid(True)
 
-
-# jerk = np.gradient(bearing_rate, delta_time)
-
-# plt.subplot(5, 1, 5)
-# plt.plot(np.arange(time_steps) * delta_time, jerk, label='Jerk to Ship A')
+# bearing_rate = np.gradient(bearings, delta_time)
+# # Plotting the bearing rate
+# plt.subplot(5, 1, 4)
+# plt.plot(np.arange(time_steps) * delta_time, bearing_rate, label='Bearing Rate to Ship A')
 # plt.xlabel('Time (s)')
-# plt.ylabel('Jerk (degrees/s^2)')
-# plt.title('Jerk to Ship A Over Time')
+# plt.ylabel('Bearing Rate (degrees/s)')
+# plt.title('Bearing Rate to Ship A Over Time')
 # plt.legend()
 # plt.grid(True)
 
-plt.subplot(5, 1, 5)
+plt.subplot(6, 1, 5)
 plt.plot(np.arange(time_steps) * delta_time, bearings_difference, label='Bearing Difference')
 plt.xlabel('Time (s)')
-plt.ylabel('Bearing Difference (degrees)')
+plt.ylabel('Bearing Difference (degrees/s)')
 plt.title('Bearing Difference Over Time')
 plt.legend()
 plt.grid(True)
+
+
+jerk = np.gradient(bearings_difference, delta_time)
+
+plt.subplot(6, 1, 6)
+plt.plot(np.arange(time_steps) * delta_time, jerk, label='Jerk to Ship A')
+plt.xlabel('Time (s)')
+plt.ylabel('Jerk (degrees/s^2)')
+plt.title('Jerk to Ship A Over Time')
+plt.legend()
+plt.grid(True)
+
+
 
 
 
@@ -248,19 +318,49 @@ plt.grid(True)
 
 # Plot ego-center map
 plt.figure(figsize=(10, 6))
+# Relative positions
+rel_x = ship_positions[:, 1] - ownship_positions[:, 1]
+rel_y = ship_positions[:, 0] - ownship_positions[:, 0]
+
 # Plotting ego-center map
-plt.plot(ship_positions[:, 1] - ownship_positions[:, 1], ship_positions[:, 0] - ownship_positions[:, 0], label='Ship A')
+plt.plot(rel_x, rel_y, label='Ship A', color='red')
+
+# Add an arrow on the last segment
+plt.annotate(
+    '',
+    xy=(rel_x[-1], rel_y[-1]),
+    xytext=(rel_x[-2], rel_y[-2]),
+    arrowprops=dict(arrowstyle='->', color='red')
+)
+
 plt.xlabel('Relative East (m)')
 plt.ylabel('Relative North (m)')
 plt.title('Ego-Center Map of Ship Positions Over Time')
 plt.legend()
 plt.grid(True)
-plt.axis('equal')  # Ensure the aspect ratio is equal
+plt.axis('equal')
 
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(ownship_positions[:, 1], ownship_positions[:, 0], label='Ownship')
-plt.plot(ship_positions[:, 1], ship_positions[:, 0], label='Ship A')
+ownship_line, = plt.plot(ownship_positions[:, 1], ownship_positions[:, 0], label='Ownship')
+ship_line,   = plt.plot(ship_positions[:, 1],  ship_positions[:, 0],  label='Ship A')
+
+# Add arrow for Ownship
+plt.annotate(
+    '',
+    xy=(ownship_positions[-1, 1], ownship_positions[-1, 0]),
+    xytext=(ownship_positions[-2, 1], ownship_positions[-2, 0]),
+    arrowprops=dict(arrowstyle='->', color=ownship_line.get_color())
+)
+
+# Add arrow for Ship A
+plt.annotate(
+    '',
+    xy=(ship_positions[-1, 1], ship_positions[-1, 0]),
+    xytext=(ship_positions[-2, 1], ship_positions[-2, 0]),
+    arrowprops=dict(arrowstyle='->', color=ship_line.get_color())
+)
+
 plt.xlabel('East (m)')
 plt.ylabel('North (m)')
 plt.title('Ship Positions Over Time in NED Coordinates (Rotated 90 Degrees)')
