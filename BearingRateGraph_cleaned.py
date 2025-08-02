4f9a96dd7b013a4484d3782f7c96e7f12ec187ff
re1983 <re1983@msn.com>
2025-08-01 18:04:20 -0600

    Lab PC

--- BearingRateGraph_cleaned.py
+++ BearingRateGraph_cleaned.py
@@ -137,9 +137,9 @@ def adj_ownship_heading(absolute_bearings, absolute_bearings_difference, angular
 
 def run_simulation():
     # Initialize ownship and target ship statuses
-    ownship = ShipStatus("Ownship", velocity=1.0, acceleration=0, heading=0, rate_of_turn=0, position=[0, 0, 0])
+    ownship = ShipStatus("Ownship", velocity=1.0, acceleration=0, heading=0, rate_of_turn=0, position=[0, 0, 0], size=0.5)
     # Target ship (Ship A) with initial position and velocity
-    ship = ShipStatus("Ship A", velocity=1.0, acceleration=0, heading=-180.0, rate_of_turn=0, position=[50, -1, 0])
+    ship = ShipStatus("Ship A", velocity=1.0, acceleration=0, heading=-180.0, rate_of_turn=0, position=[50, -1, 0], size=0.5)
     # Goal ship for navigation
     goal = ShipStatus("Goal", velocity=0.0, acceleration=0, heading=0, rate_of_turn=0, position=[50, 0, 0])
     time_steps = 5000
